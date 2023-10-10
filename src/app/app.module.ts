f8db929fa743cf81d3b7a35f2efd210e8e9aa973
crsayuri <cristina.sayuri@gmail.com>
2023-10-10 15:52:16 -0300

    components and style creation

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
+import { MainCardComponent } from './components/main-card/main-card.component';
+import { SidebarCardComponent } from './components/sidebar-card/sidebar-card.component';
+import { NormalCardComponent } from './components/normal-card/normal-card.component';
+import { HeroTitleComponent } from './components/hero-title/hero-title.component';
+import { HomeComponent } from './pages/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    MainCardComponent,
+    SidebarCardComponent,
+    NormalCardComponent,
+    HeroTitleComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/sidebar-card/sidebar-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-card',
+  templateUrl: './sidebar-card.component.html',
+  styleUrls: ['./sidebar-card.component.css']
+})
+export class SidebarCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
